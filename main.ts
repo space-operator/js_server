7ca4391385315a07c1d0ae917582daf3c970534b
enzotar <enzotar3000@gmail.com>
2023-12-28 18:16:04 -0800

    .

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { Hono } from 'https://deno.land/x/hono@v3.4.1/mod.ts';
-import data from './data.json' assert { type: 'json' };
 import { create_wrapped_on_eth } from './create_wrapped_on_eth.ts';
 
 const app = new Hono();
@@ -12,14 +11,14 @@ app.post('/api/create_wrapped_on_eth', async (c) => {
   create_wrapped_on_eth(body);
 });
 
-app.get('/api/:dinosaur', (c) => {
-  const dinosaur = c.req.param('dinosaur').toLowerCase();
-  const found = data.find((item) => item.name.toLowerCase() === dinosaur);
-  if (found) {
-    return c.json(found);
-  } else {
-    return c.text('No dinosaurs found.');
-  }
-});
+// app.get('/api/:dinosaur', (c) => {
+//   const dinosaur = c.req.param('dinosaur').toLowerCase();
+//   const found = data.find((item) => item.name.toLowerCase() === dinosaur);
+//   if (found) {
+//     return c.json(found);
+//   } else {
+//     return c.text('No dinosaurs found.');
+//   }
+// });
 
 Deno.serve(app.fetch);
