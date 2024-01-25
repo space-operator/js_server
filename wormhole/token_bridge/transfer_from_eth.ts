cc1b301c3c75a8afa999f414bbc7ccd485473af5
enzotar <enzotar3000@gmail.com>
2024-01-25 15:49:14 -0800

    fix env

--- wormhole/token_bridge/transfer_from_eth.ts
+++ wormhole/token_bridge/transfer_from_eth.ts
@@ -41,8 +41,7 @@ export async function transfer_from_eth(event: any) {
 
   // Setup Provider
   const settings = {
-    apiKey: env['ALCHEMY_API_KEY'],
-    // apiKey: Deno.env.get('ALCHEMY_API_KEY'),
+    apiKey: Deno.env.get('ALCHEMY_API_KEY') ?? env['ALCHEMY_API_KEY'],
     network,
   };
   const alchemy = new Alchemy(settings);
